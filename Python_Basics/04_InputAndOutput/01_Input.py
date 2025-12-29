63815efeca8cc3e61d2a551c6cc59627f7644dbc
ErGovindthakur <ergovindthakur@gmail.com>
2025-12-29 15:26:34 +0530

    learn about how get output in formated way

---
+++ Python_Basics/04_InputAndOutput/01_Input.py
@@ -0,0 +1,23 @@
+# ✅ What is Output?
+
+# Output means showing result to the user.
+
+# In Python, we use print() to display output.
+
+print('Hello World!') # output = Hello World!
+
+
+# printing variables
+myName = "Govind"
+herName = "Prabhjot"
+
+print(myName)
+print(herName)
+
+# Printing Multiple Values
+print(myName,"and",herName)
+
+
+# 🔹 Printing Multiple Values (Using f-strings)
+print(f"{herName}-: Hello {myName}")
+print(f"{myName}-: Hi {herName}, How are you ?")

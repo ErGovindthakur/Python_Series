67bd5df4eaadaa7b79969041f3843e191a3c3b5a
ErGovindthakur <ergovindthakur@gmail.com>
2025-12-29 15:33:17 +0530

    explore how to take input in python and return it in more formated way

---
+++ Python_Basics/04_InputAndOutput/02_Input.py
@@ -0,0 +1,19 @@
+# ✅ What is Input?
+
+# Input means taking data from the user.
+
+# Python uses "input()".
+
+name = input("Enter your name ")
+print(name)
+
+# Note -> input() always returns a string
+
+print(type(name))
+
+# Small Practical Example with type conversion
+
+myName = input('Enter your name ')
+myAge = int(input("Enter your age "))
+
+print(f"Hello, {myName} you are {myAge}yrs old")

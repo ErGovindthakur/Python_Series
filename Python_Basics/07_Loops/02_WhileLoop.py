2821cbfd0906d4367db2640c52076ebf617a7ab8
ErGovindthakur <ergovindthakur@gmail.com>
2025-12-31 17:03:02 +0530

    improve readability

--- Python_Basics/07_Loops/02_WhileLoop.py
+++ Python_Basics/07_Loops/02_WhileLoop.py
@@ -11,13 +11,13 @@ Stops when user enters a negative number
 Prints the sum of all positive numbers entered
 '''
 
-sum = 0
+total = 0
 x = 1
 while x != 0:
      x = int(input("Enter a number , to stop enter negative number -> "))
      if(x > 0):
-          sum += x
+          total += x
      else:
-          print("Sum of All positive number ->",sum)
+          print("Sum of All positive number ->",total)
           break
      

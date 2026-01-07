4cf0f30e36e47f752aa404c42bfe07bf6afa3546
ErGovindthakur <ergovindthakur@gmail.com>
2026-01-07 14:40:41 +0530

    explore polymorphism in python

--- Python_Basics/12_OOPS/03_Polymorphism/Polymorphism.py
+++ Python_Basics/12_OOPS/03_Polymorphism/Polymorphism.py
@@ -22,3 +22,41 @@ shapes = [Square(),Circle()]
 
 for shape in shapes:
      shape.area()
+
+
+# 2. Example using phone unlock methods
+
+# python polymorphism using method overriding
+
+class PhoneUnlock:
+    def Unlock(self):
+        print("Phone Unlocked by following methods.....")
+
+class FaceUnlock(PhoneUnlock):
+    def Unlock(self):
+        super().Unlock()
+        print("Phone Unlocked by face.....")
+        
+class PinUnlock(PhoneUnlock):
+    def Unlock(self):
+        print("Phone Unlocked by pin.....")
+        
+class FingerUnlock(PhoneUnlock):
+    def Unlock(self):
+        print("Phone Unlocked by finger print.....")
+        
+        
+class PatternUnlock(PhoneUnlock):
+    def Unlock(self):
+        print("Phone Unlocked by pattern.....")
+        
+    
+    
+pnUnlck = [FingerUnlock(),PinUnlock(),FaceUnlock(),PatternUnlock()]
+
+
+for unlck in pnUnlck:
+    unlck.Unlock()
+    
+    
+    

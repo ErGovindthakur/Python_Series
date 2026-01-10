ec7ddaad37063340a02b343871936123500cc158
ErGovindthakur <ergovindthakur@gmail.com>
2026-01-10 18:48:57 +0530

    finally solved this problem in O(n) tmc

--- DSA_PYTHON/01_Array/PythonProblem.py
+++ DSA_PYTHON/01_Array/PythonProblem.py
@@ -2,17 +2,24 @@
 
 # 1. Find duplicate elem from the list
 
-def findDup(list):
-     if len(list) < 2:
-          return list
+def findDup(ls):
+     if len(ls) < 2:
+          return ls
      
      dupTracker = []
-
-     for i in range(len(list)):
-          for j in range(len(list)):
-               if i != j and list[i] == list[j]:
-                    if list[i] not in dupTracker:
-                         dupTracker.append(list[i])
+     freq = {}
+     # for i in range(len(list)):
+     #      for j in range(len(list)):
+     #           if i != j and list[i] == list[j]:
+     #                if list[i] not in dupTracker:
+     #                     dupTracker.append(list[i])
+     for num in ls:
+          if num in freq:
+               freq[num] +=1
+               if freq[num] == 2:
+                    dupTracker.append(num)
+          else:
+               freq[num] = 1
      return dupTracker
 
 print(findDup([1,2,3,1,4,5,3,2]))
